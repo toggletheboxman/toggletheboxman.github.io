0ef361e2c3b0e853409980e1fe85aa4c6fa0211e
Chafic LaRochelle-Eid <chafic.laro@gmail.com>
2023-02-15 15:10:55 -0500

    d

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <body>
+      <main>
+        <p>SEND E-MAIL</p>
+        <form class="contact-form" action="contactform.php" method="post">
+<input type="text" name="name" placeholder="Full Name">
+<input type="text" name="mail" placeholder="Your E-Mail">
+<input type="text" name="subject" placeholder="Subject">
+<textarea name="message" placeholder="Message"></textarea>
+<button type="submit" name="submit">SEND MAIL</button>
+        </form>
+      </main>
+    </body>
+  </head>
+</html>
