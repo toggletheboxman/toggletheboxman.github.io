44993112c5970c0f7db44e4460eeff634f984f3f
Chafic LaRochelle-Eid <chafic.laro@gmail.com>
2023-02-15 16:55:59 -0500

    closer

--- contactform.php
+++ contactform.php
@@ -6,5 +6,10 @@ if (isset($_POST['submit'])) {
   $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
 
-  mail();
+  $mailTo = "chafic.laro@gmail.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an e-mail from ".$name. ".\n\n".$message;
+
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: index.php?mailsend");
 }
